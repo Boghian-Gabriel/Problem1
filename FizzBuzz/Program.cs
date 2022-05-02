dfcc1123e55b886fd9ac46635e8b6c21938e7312
Admin <Admin@PC162>
2022-05-02 09:34:40 +0300

    [ADD] - FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            // step 1 : Print number from 1 to n
+            for(int i=0; i <= n; i++)
+            {
+                // step2: Divizible by 3 prin Fizz
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                // step3 : Divizible by 5 prin Buzz
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                //Step4 : Divizible by 3 and 5 print FizzBuzz
+                else if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    // Display the numbers...
+                    Console.WriteLine(i);
+                }
+            }
+            //Thne console can wait for a push of a button
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ NFactorial/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This program that return the prime number which you entere form keyboard!
+            Console.WriteLine("Enter a number: ");
+            //Initialize n variable
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            //fact variable start at 1, because all number multiple by 1 rezult there number.
+            int fact = 1;
+            int contor = 0;
+            for(int i = 1; i <=n; i++)
+            {
+                contor++;
+                Console.WriteLine("Step " + contor + " => " + i);
+                fact = fact * i;
+            }
+            Console.WriteLine("The number rezult" + n + "is: " + fact);
+
+            //reinitialize
+            contor = 0;
+            fact = 1;
+            Console.WriteLine("Enter another number: ");
+            int ii = 1;
+            while (ii <= m) {
+                contor++;
+                Console.WriteLine("Step " + contor + " => " + ii);
+                fact = fact * ii;
+
+                ii = ii + 1;
+            }
+            Console.WriteLine("The number rezult" + m + "is: " + fact);
+            Console.ReadKey();
+        }
+    }
+}
