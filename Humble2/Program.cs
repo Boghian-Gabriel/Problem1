9b9f1b806a0f2711c35fce226d00ff528b30c23e
Admin <Admin@PC162>
2022-05-12 14:30:48 +0300

    [Add] method isPalindrom

---
+++ Humble2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isPalindrom
+{
+    internal class Program
+    {
+
+        static bool isPalindrom(string inputString)
+        {
+            string reverse = string.Empty;
+
+            for (int i = inputString.Length - 1; i>=0; i--) 
+            {
+                reverse += inputString[i];    
+            }
+            if(inputString.Equals(reverse, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter a string: ");
+            string s = Console.ReadLine();
+            isPalindrom(s);
+            Console.ReadKey();
+        }
+    }
+}
