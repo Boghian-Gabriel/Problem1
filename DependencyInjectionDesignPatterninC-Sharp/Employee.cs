329f5ee21879782e867695a00d1d9b1987d74a32
Admin <Admin@PC162>
2022-05-30 12:27:26 +0300

    [Add] -

---
+++ DependencyInjectionDesignPatterninC-Sharp/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDesignPatterninC_Sharp
+{
+    public class Employee
+    {
+        //properties with accesors
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public string Department {get; set;}
+    }
+}
--- FizzBuzzWithList/Program.cs
+++ FizzBuzzWithList/Program.cs
@@ -67,4 +67,4 @@ namespace FizzBuzzWithList
             FizzBuzz(list2);
         }
     }
-}N
+}
