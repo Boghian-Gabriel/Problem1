a304e01d2795d3cba188b5ba3e40b74691288da4
Admin <Admin@PC162>
2022-05-30 14:28:19 +0300

    [Add] string replace

---
+++ EliminateA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliminateA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+
+            string strCopy = str.Replace('a', '@');
+
+            Console.Write(strCopy);
+
+
+            string[] words = strCopy.Split(' ');
+            Console.WriteLine('\n');
+            foreach (var word in words)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
