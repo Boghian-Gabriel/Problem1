d1c010f6a47371afe84ed3b0d99f5a693fae8a7d
Admin <Admin@PC162>
2022-05-31 10:20:56 +0300

    [Add] Singleton Aspects

---
+++ SingletonClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton Design Pattern in C#.
+
+            //Now. Let us modify the main method of the program
+            //class to access the nested class as shown below.
+
+            //private construcor is called before we instantiated
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+
+            /*
+             * Instantiating singleton from a derived class.
+             * This violates singleton pattern principles.
+             */
+            //Singleton.DerivedSingleton derivedSingleton = new Singleton.DerivedSingleton();
+            //derivedSingleton.PrintDetails("From Derived");
+        }
+    }
+}
---
+++ SingletonClass/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClass
+{
+    internal sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value: " + counter.ToString());
+
+        }
+        
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        //What is a nested class in C#?
+        // Whenever we defined a class within another class in C# then the inner
+        // class is called a nested class or child class.
+        
+        //When the parent class is sealed, we can not uses inheritance
+        //public class DerivedSingleton : Singleton
+        //{
+
+        //}
+    }
+}
