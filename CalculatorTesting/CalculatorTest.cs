0cf9becd97284601e42e7a05a60af7a90e0f4469
Admin <Admin@PC162>
2022-06-06 09:40:10 +0300

    [Add] example Unit Testing

---
+++ CalculatorTesting/CalculatorTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTesting
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Test_Divide()
+        {
+            //1. Arrange = initializez objects and set the value of the data
+            // that is passed to the method being tested
+            int expected = 10;
+            int numerator = 100;
+            int denominator = 10;
+            
+            //2. Act = invokes the method being tested
+            int actual = Calculator.Program.Divide(numerator, denominator);
+            
+            //3. Asset = verifies that the method being tested
+            //         behaves as expected
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_PozNumeratorAndNegativeDenominator_ReturnsNegativeQuotient()
+        {
+            // Arrange
+            int expected = -10;
+            int numerator = 100;
+            int denominator = -10;
+            // Act
+            int actual = Calculator.Program.Divide(numerator, denominator);
+            // Asset
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_NegativeNumber_ReturnPositiveQuotient()
+        {
+            // Arrange
+            int expected = 10;
+            int numerator = -100;
+            int denominator = -10;
+            // Act
+            int actual = Calculator.Program.Divide(numerator , denominator);
+            // Asset
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException), "Try to divide numerator by zero")]
+        public void Divide_DenominatorIsZero_ThrowDivideByZeroException()
+        {
+            // Arrange   
+            int except = 0;
+            int numerator = 100;
+            int denominator = 0;
+            // Act            
+
+            int result = Calculator.Program.Divide(numerator, denominator);
+            
+            //Assert
+            Assert.AreEqual(except, result);
+            /*
+             We can run the unit test in several ways,
+            1. Run all test
+            2. Run single test
+            3. Run all test for the only selected test class.
+             */
+        }
+    }
+}
---
+++ UnitTesting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        /*
+         Popular Unit Testing frameworks
+            1. MSTEST
+            2. Nunit
+            3. MbUNit
+            4. net
+        MSTEST is by default integrated with VS
+         */
+        public static int Divide(int numerator, int denominator)
+        {
+            int rezult = numerator / denominator;
+            return rezult;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
