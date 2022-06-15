36cf88a991f70bfbb78c6894eb77609e81f1c0f5
Admin <Admin@PC162>
2022-06-15 12:02:14 +0300

    [Add] Ref and Out

--- ImplicitAndExplicitInterface/Program.cs
+++ ImplicitAndExplicitInterface/Program.cs
@@ -20,9 +20,43 @@ namespace ImplicitAndExplicitInterface
 
 
             // 2. With explicit interface
-            Itest2 obj = new TestWithExplicitInterface();
-            obj.TestMethod2();
-            Console.ReadKey();
+            //Itest2 obj = new TestWithExplicitInterface();
+            //obj.TestMethod2();
+            //Console.ReadKey();
+
+            //Difference between  out and ref
+            // out  return multiple value
+            // ref - schimbarea se resimte si la declarare
+            int G;
+            int G2 = 0;
+            
+
+            Sum(out G);
+            Sum2(ref G2);
+            Console.WriteLine(G2);
+            // Display the value G
+            Console.WriteLine("The sum of" +
+                    " the value is: {0}", G);
+
+            Console.WriteLine("The sum of" +
+                    " the value is: {0}", G2);
+        }
+        public static void Sum(out int G)
+        {
+            G = 10;
+            G++;
+
+        }
+
+        public static void Sum2(ref int G)
+        {
+            // Check parameter value
+            if (G == 0)
+            {
+                Console.WriteLine("It's true!");
+            }
+
+            G = 20;
         }
     }
 }
