0e37efcb2971632f787828f0784f8ed9e22d8880
Admin <Admin@PC162>
2022-06-22 10:37:16 +0300

    [Add] logger

---
+++ DepedencyInjection/Program.cs
@@ -0,0 +1,32 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepedencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages messages = new Messages();
+            messages.Log("Example about DI");
+        }
+        //using interface
+        public interface ILogger
+        {
+            void Log(string message);
+        }
+
+        public class Messages : ILogger
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine($"This message is : {message}");
+            }
+        }
+
+       
+    }
+}
