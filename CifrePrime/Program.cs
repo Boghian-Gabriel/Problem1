9e6d16d619bd2ac91ec43e953219226d0b7a9b98
Admin <Admin@PC162>
2022-06-27 13:45:26 +0300

    [Add] Cifre Prime and so on

--- CifrePrime/Program.cs
+++ CifrePrime/Program.cs
@@ -15,18 +15,44 @@ namespace CifrePrime
             Console.WriteLine();
             //List cu cifrele unui numar
             List<int> nrCifreList = new List<int>();
+            List<int> lstcifrelePrimeDinNumar = new List<int>();
             //Cifrele unui numar:
             int uc = 0;
-            int nrCifre = 0, cNrPrime = 0 ;
+            int nrCifre = 0, cNrDiv = 0, cNrPrime = 0 ;
             bool isNrPrim = false;
             int copie = number;
+            
             while(copie > 0)
             {
                 uc = copie % 10;
                 //Console.Write("{0} ", uc);
-                if (uc % 2 == 0)
-                {
-                    isNrPrim = true;
+                if (uc > 0) {
+                    //int div = 1;
+                    int div;
+                    cNrDiv = 0;
+                    //while (div <= uc)
+                    //{
+                    //    if(uc %  div == 0)
+                    //    {
+                    //        cNrDiv++;
+                    //    }                        
+                    //    div++;
+                    //}
+                    for(div =1; div <= uc; div++)
+                    {
+                        if(uc % div == 0)
+                        {
+                            cNrDiv++;
+                        }
+                    }
+                    //Daca are numar = 2 de divizori (daca se imaprte la 1 si la el insuri atunci este prim)
+                   if (cNrDiv == 2)
+                    {
+                        isNrPrim = true;
+                        cNrPrime++;
+                    lstcifrelePrimeDinNumar.Add(uc);
+                    lstcifrelePrimeDinNumar.Sort();
+                    }
                 }
                 nrCifreList.Add(uc);
                 nrCifre++;
@@ -34,12 +60,23 @@ namespace CifrePrime
             }
             Console.WriteLine("\nNumarul {0} are {1} cifre ", number, nrCifre);
 
-            Console.WriteLine("\nCifrele numarului sunt: ");
+            Console.Write("\nCifrele numarului sunt: ");
             foreach(var item in nrCifreList)
             {
                 Console.Write(item + " ");
             }
 
+            if (isNrPrim)
+            {
+                Console.WriteLine($"\nNumarul {number} are {cNrPrime} cifre prime!");
+            }
+
+
+            Console.Write($"\nCifrele prime ale numarului {number} sunt: ");
+            foreach (var cifrePrime in lstcifrelePrimeDinNumar)
+            {
+                Console.Write(cifrePrime + " ");
+            }
             Console.ReadKey();
         }
     }
