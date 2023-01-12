13bd6648814e4fe689b76a3e76112bf935139aa3
Boghian Gabriel <boghiangabriel96@yahoo.com>
2023-01-12 08:56:33 +0200

    [add] Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        //verificam daca numarul este patrat perfect atunci true, else false
+        static bool isPerfectSquare(int x)
+        {
+            int s = (int)Math.Sqrt(x);
+            return (s * s == x);
+        }
+
+        //una din metode ce verifica pe baza functiei de mai sus daca nr este Fibonacci
+        static bool itemIsFibonacci(int number)
+        {
+            return isPerfectSquare(5 * number * number + 4) ||
+                    isPerfectSquare(5 * number * number - 4);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu numarul de elemente din vector: ");
+            int nr = int.Parse(Console.ReadLine());
+            int[] v = new int[nr];
+
+            Console.Write("Introdu elementele din vector:\n ");
+            for (int i = 0; i < nr; i++)
+            {
+                Console.Write($"v[{i}] = ");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("\nVectorul initial este: ");
+            for (int i = 0; i < nr; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+
+            List<int> lstWithFibonnaciItem = new List<int>();
+            int iContor = 0;
+            //verific daca fiecare element din vector face parte din sirul fibonnaci
+            for (int i = 0; i < nr; i++)
+            {
+                int item = v[i];
+                if (itemIsFibonacci(v[i]))
+                {
+                    iContor++;
+                    lstWithFibonnaciItem.Add(item);
+                }
+            }
+
+            Console.Write($"\nIn vectorul alcatuit anterior, sunt {iContor} numere Fibonacci, iar acestea sunt: ");
+            foreach (var item in lstWithFibonnaciItem)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
