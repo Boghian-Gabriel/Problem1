98cc0fc0d1ca4fde14bdb70ff0fee469d917055a
gabriel <boghiangabriel96@yahoo.com>
2024-05-17 09:44:27 +0300

    [Modify] Change some component from UI [Add] New font

---
+++ RichTextBox/ToolTipInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace RichTextBox
+{
+    public static class ToolTipInformation
+    {
+        public static void AddToolTip(Button button, string info)
+        {
+            ToolTip toolTip = new ToolTip();
+
+            toolTip.SetToolTip(button, info);
+        }
+    }
+}
